d100134425669902cbe358861ca86d966b5c113a
RealKiro <zuoqihe@gmail.com>
2019-12-17 14:12:55 +0800

    re-add smartdns from official sources

--- diy.sh
+++ diy.sh
@@ -9,9 +9,9 @@
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认主题为Argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
-# git clone https://github.com/jerrykuku/luci-theme-argon.git package/mine/luci-theme-argon_new
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/mine/luci-theme-argon_new
+sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/Makefile
 
 # 复杂的AdGuardHome的openwrt的luci界面
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/mine/luci-app-adguardhome
@@ -21,6 +21,12 @@ git clone https://github.com/Ameykyl/luci-app-koolproxyR.git package/mine/luci-a
 
 # Server酱
 git clone https://github.com/tty228/luci-app-serverchan package/mine/luci-app-serverchan
+
+# SmartDNS
+git clone https://github.com/pymumu/luci-app-smartdns.git package/mine/SmartDNS/luci-app-smartdns
+git clone https://github.com/pymumu/smartdns.git package/mine/SmartDNS/smartdns
+sed -i 's/+luci-compat //g' package/mine/SmartDNS/luci-app-smartdns/Makefile
+sed -i 's/include ../../luci.mk/include $(TOPDIR)/feeds/luci/luci.mk/g' package/mine/SmartDNS/luci-app-smartdns/Makefile
 
 # 硬盘监控
 git clone https://github.com/project-openwrt/luci-app-smartinfo package/mine/luci-app-smartinfo
